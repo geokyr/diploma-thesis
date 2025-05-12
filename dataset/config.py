7fa876986c6607ca486bd14e41dee9052736ae9c
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-05-12 21:20:22 +0300

    updated order and separation in groups of constants

--- dataset/config.py
+++ dataset/config.py
@@ -28,30 +28,33 @@ FIXED_ROUTES_FILE = DATA_DIR / "fixed.rou.xml"
 FIXED_ROUTES_ALT_FILE = DATA_DIR / "fixed.rou.alt.xml"
 
 BASE_TRAIN_TRIPS_FILE = DATA_DIR / "base-train.trips.xml"
-BASE_TEST_TRIPS_FILE = DATA_DIR / "base-test.trips.xml"
 BASE_TRAIN_SIMULATION_CONFIG = DATA_DIR / "base-train.sumocfg"
-BASE_TEST_SIMULATION_CONFIG = DATA_DIR / "base-test.sumocfg"
 BASE_TRAIN_FCD = DATA_DIR / "base-train-fcd.xml"
-BASE_TEST_FCD = DATA_DIR / "base-test-fcd.xml"
 BASE_TRAIN_DATASET_ID = "base-train"
+
+BASE_TEST_TRIPS_FILE = DATA_DIR / "base-test.trips.xml"
+BASE_TEST_SIMULATION_CONFIG = DATA_DIR / "base-test.sumocfg"
+BASE_TEST_FCD = DATA_DIR / "base-test-fcd.xml"
 BASE_TEST_DATASET_ID = "base-test"
 
 CLOSURE_TRAIN_TRIPS_FILE = DATA_DIR / "closure-train.trips.xml"
-CLOSURE_TEST_TRIPS_FILE = DATA_DIR / "closure-test.trips.xml"
 CLOSURE_TRAIN_SIMULATION_CONFIG = DATA_DIR / "closure-train.sumocfg"
-CLOSURE_TEST_SIMULATION_CONFIG = DATA_DIR / "closure-test.sumocfg"
 CLOSURE_TRAIN_FCD = DATA_DIR / "closure-train-fcd.xml"
-CLOSURE_TEST_FCD = DATA_DIR / "closure-test-fcd.xml"
 CLOSURE_TRAIN_DATASET_ID = "closure-train"
+
+CLOSURE_TEST_TRIPS_FILE = DATA_DIR / "closure-test.trips.xml"
+CLOSURE_TEST_SIMULATION_CONFIG = DATA_DIR / "closure-test.sumocfg"
+CLOSURE_TEST_FCD = DATA_DIR / "closure-test-fcd.xml"
 CLOSURE_TEST_DATASET_ID = "closure-test"
 
 RAIN_TRAIN_TRIPS_FILE = DATA_DIR / "rain-train.trips.xml"
-RAIN_TEST_TRIPS_FILE = DATA_DIR / "rain-test.trips.xml"
 RAIN_TRAIN_SIMULATION_CONFIG = DATA_DIR / "rain-train.sumocfg"
-RAIN_TEST_SIMULATION_CONFIG = DATA_DIR / "rain-test.sumocfg"
 RAIN_TRAIN_FCD = DATA_DIR / "rain-train-fcd.xml"
-RAIN_TEST_FCD = DATA_DIR / "rain-test-fcd.xml"
 RAIN_TRAIN_DATASET_ID = "rain-train"
+
+RAIN_TEST_TRIPS_FILE = DATA_DIR / "rain-test.trips.xml"
+RAIN_TEST_SIMULATION_CONFIG = DATA_DIR / "rain-test.sumocfg"
+RAIN_TEST_FCD = DATA_DIR / "rain-test-fcd.xml"
 RAIN_TEST_DATASET_ID = "rain-test"
 
 TRAIN_TRAFFIC_GENERATION_PERIODS = [0.45, 0.50, 0.65, 0.75, 0.80, 0.80, 0.75, 0.55, 0.50, 0.55]
