d13a190dd20f728d3c43c4cecfba6f00cf37ee31
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-05-13 15:12:32 +0300

    changed pd.dataframe to pd.series for visualization

--- dataset/visualization.py
+++ dataset/visualization.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_speed_histogram(df_speed_kmh: pd.DataFrame, dataset_id: str, bins: int = 30) -> None:
+def plot_speed_histogram(df_speed_kmh: pd.Series, dataset_id: str, bins: int = 30) -> None:
     """
     Plot a histogram of vehicle speeds in km/h.
 
     Args:
-        df (pd.DataFrame): DataFrame containing vehicle speed data in km/h.
+        df_speed_kmh (pd.Series): Series containing vehicle speed data in km/h.
         dataset_id (str): Dataset ID to identify the dataset.
         bins (int): Number of bins for the histogram.
     """
@@ -20,18 +20,18 @@ def plot_speed_histogram(df_speed_kmh: pd.DataFrame, dataset_id: str, bins: int 
 
 
 def plot_average_speed_and_vehicle_count_per_second(
-    df_second: pd.DataFrame,
-    df_average_speed_kmh_per_second: pd.DataFrame,
-    df_vehicle_count_per_second: pd.DataFrame,
+    df_second: pd.Series,
+    df_average_speed_kmh_per_second: pd.Series,
+    df_vehicle_count_per_second: pd.Series,
     dataset_id: str,
 ) -> None:
     """
     Plot average speed and vehicle count on a per-second basis.
 
     Args:
-        df_second (pd.DataFrame): DataFrame containing second-wise data.
-        df_average_speed_kmh_per_second (pd.DataFrame): DataFrame containing average speed per hour in km/h.
-        df_vehicle_count_per_second (pd.DataFrame): DataFrame containing vehicle count per second.
+        df_second (pd.Series): Series containing second-wise data.
+        df_average_speed_kmh_per_second (pd.Series): Series containing average speed per hour in km/h.
+        df_vehicle_count_per_second (pd.Series): Series containing vehicle count per second.
         dataset_id (str): Dataset ID to identify the dataset.
     """
     fig, ax1 = plt.subplots(figsize=(10, 4))
@@ -52,8 +52,8 @@ def plot_average_speed_and_vehicle_count_per_second(
 
 
 def plot_average_speed_and_traffic_generation_period_per_hour(
-    df_hour: pd.DataFrame,
-    df_average_speed_kmh_per_hour: pd.DataFrame,
+    df_hour: pd.Series,
+    df_average_speed_kmh_per_hour: pd.Series,
     traffic_generation_periods: list[int],
     dataset_id: str,
 ) -> None:
@@ -61,8 +61,8 @@ def plot_average_speed_and_traffic_generation_period_per_hour(
     Plot average speed and traffic generation period on a per-hour basis.
 
     Args:
-        df_hour (pd.DataFrame): DataFrame containing hour-wise data.
-        df_average_speed_kmh_per_hour (pd.DataFrame): DataFrame containing average speed per hour in km/h.
+        df_hour (pd.Series): Series containing hour-wise data.
+        df_average_speed_kmh_per_hour (pd.Series): Series containing average speed per hour in km/h.
         traffic_generation_periods (list[int]): List of traffic generation periods in seconds.
         dataset_id (str): Dataset ID to identify the dataset.
     """
