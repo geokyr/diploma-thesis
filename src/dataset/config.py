77c78cbda8c3572fa717a0074f9446e5e03a5a06
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-06-07 15:40:40 +0300

    updated sumo env exception

--- src/dataset/config.py
+++ src/dataset/config.py
@@ -10,11 +10,12 @@ TEST_SEED = 123
 TRAIN_TRAFFIC_GENERATION_PERIODS = [0.45, 0.50, 0.65, 0.75, 0.80, 0.80, 0.75, 0.55, 0.50, 0.55]
 TEST_TRAFFIC_GENERATION_PERIODS = [p * np.random.uniform(0.98, 1.02) for p in TRAIN_TRAFFIC_GENERATION_PERIODS]
 
-SUMO_HOME = Path(os.environ.get("SUMO_HOME", None))
-if SUMO_HOME is None:
+SUMO_HOME_ENV = os.environ.get("SUMO_HOME", None)
+if SUMO_HOME_ENV is None:
     raise EnvironmentError(
         "Please set the SUMO_HOME environment variable. Also add SUMO_HOME/bin and SUMO_HOME/tools to your PATH."
     )
+SUMO_HOME = Path(SUMO_HOME_ENV)
 
 OSM_WEB_WIZARD = SUMO_HOME / "tools" / "osmWebWizard.py"
 RANDOM_TRIPS = SUMO_HOME / "tools" / "randomTrips.py"
