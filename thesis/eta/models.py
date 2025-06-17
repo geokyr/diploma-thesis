86aeb3376927ccbe56215e43ea60fdd257be1bb6
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-06-18 01:36:24 +0300

    simplified the models file and rerun

--- thesis/eta/models.py
+++ thesis/eta/models.py
@@ -1,6 +1,3 @@
-from collections.abc import Callable
-from typing import Any
-
 from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor
 from sklearn.base import BaseEstimator
@@ -11,9 +8,9 @@ from xgboost import XGBRegressor
 from thesis.eta.config import RANDOM_STATE
 
 
-def create_linear_regression_model(**kwargs) -> LinearRegression:
+def create_lr_model(**kwargs) -> LinearRegression:
     """
-    Create a Linear Regression model with baseline configuration.
+    Create a Linear Regression model with a default configuration.
 
     Args:
         **kwargs: Additional parameters to pass to the model.
@@ -26,7 +23,7 @@ def create_linear_regression_model(**kwargs) -> LinearRegression:
 
 def create_mlp_model(**kwargs) -> MLPRegressor:
     """
-    Create a Multi-Layer Perceptron model with baseline configuration.
+    Create a Multi-Layer Perceptron model with a default configuration.
 
     Args:
         **kwargs: Additional parameters to pass to the model.
@@ -39,7 +36,7 @@ def create_mlp_model(**kwargs) -> MLPRegressor:
 
 def create_xgboost_model(**kwargs) -> XGBRegressor:
     """
-    Create an XGBoost model with baseline configuration.
+    Create an XGBoost model with a default configuration.
 
     Args:
         **kwargs: Additional parameters to pass to the model.
@@ -52,7 +49,7 @@ def create_xgboost_model(**kwargs) -> XGBRegressor:
 
 def create_lightgbm_model(**kwargs) -> LGBMRegressor:
     """
-    Create a LightGBM model with baseline configuration.
+    Create a LightGBM model with a default configuration.
 
     Args:
         **kwargs: Additional parameters to pass to the model.
@@ -65,7 +62,7 @@ def create_lightgbm_model(**kwargs) -> LGBMRegressor:
 
 def create_catboost_model(**kwargs) -> CatBoostRegressor:
     """
-    Create a CatBoost model with baseline configuration.
+    Create a CatBoost model with a default configuration.
 
     Args:
         **kwargs: Additional parameters to pass to the model.
@@ -76,32 +73,17 @@ def create_catboost_model(**kwargs) -> CatBoostRegressor:
     return CatBoostRegressor(random_state=RANDOM_STATE, verbose=0, allow_writing_files=False, **kwargs)
 
 
-BASELINE_MODELS: dict[str, Callable[..., BaseEstimator]] = {
-    "linear-regression": create_linear_regression_model,
-    "multi-layer-perceptron": create_mlp_model,
-    "xgboost": create_xgboost_model,
-    "lightgbm": create_lightgbm_model,
-    "catboost": create_catboost_model,
-}
-
-
-def get_baseline_models(model_configs: dict[str, dict[str, Any]] | None = None) -> dict[str, BaseEstimator]:
+def get_baseline_models() -> dict[str, BaseEstimator]:
     """
     Get a dictionary of baseline models.
 
-    Args:
-        model_configs (dict[str, dict[str, Any]] | None): Custom configurations for specific models.
-
     Returns:
-        dict[str, BaseEstimator]: dictionary of baseline models.
+        dict[str, BaseEstimator]: Dictionary of baseline models.
     """
-
-    if model_configs is None:
-        model_configs = {}
-
-    models = {}
-    for model_name, model_creator in BASELINE_MODELS.items():
-        custom_config = model_configs.get(model_name, {})
-        models[model_name] = model_creator(**custom_config)
-
-    return models
+    return {
+        "lr": create_lr_model(),
+        "mlp": create_mlp_model(),
+        "xgboost": create_xgboost_model(),
+        "lightgbm": create_lightgbm_model(),
+        "catboost": create_catboost_model(),
+    }
