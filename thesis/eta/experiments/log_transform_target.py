d617f1fa24cb20a2c63619ee6261af25cd5ad333
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-06-29 19:16:43 +0300

    removed the old log transform target and replaced with the wrap with transform target regressor

--- thesis/eta/experiments/log_transform_target.py
+++ thesis/eta/experiments/log_transform_target.py
@@ -2,8 +2,8 @@ from thesis.common.logger import setup_logger
 from thesis.eta.config import SCENARIOS_SPECS
 from thesis.eta.data import load_fcd_dataset, prepare_baseline_trips, preprocess_fcd_dataset
 from thesis.eta.experiment import initialize_experiment, save_model, save_results
-from thesis.eta.features import inverse_log_transform_predictions, log_transform_target, split_features_and_target
-from thesis.eta.models import get_baseline_models
+from thesis.eta.features import create_log_transformer, split_features_and_target
+from thesis.eta.models import get_baseline_models, wrap_with_transformed_target_regressor
 from thesis.eta.pipeline import evaluate_predictions, make_predictions, train_model
 
 
@@ -11,6 +11,8 @@ def main() -> None:
     experiment_name = "log_transform_target"
     artifacts_dir, logs_dir, _, results_dir = initialize_experiment(experiment_name)
     logger = setup_logger(experiment_name, logs_dir)
+
+    log_transformer = create_log_transformer()
 
     for scenario_name, train_path, test_path in SCENARIOS_SPECS:
         logger.info(f"Starting scenario {scenario_name}")
@@ -24,14 +26,14 @@ def main() -> None:
 
         X_train, y_train = split_features_and_target(trips_train)
         X_test, y_test = split_features_and_target(trips_test)
-        y_train_log = log_transform_target(y_train)
 
         models = get_baseline_models()
         results = {}
-        for model_name, model in models.items():
-            training_results = train_model(model, model_name, X_train, y_train_log)
-            predictions_log, prediction_results = make_predictions(model, model_name, X_test)
-            predictions = inverse_log_transform_predictions(predictions_log)
+
+        for model_name, base_model in models.items():
+            model = wrap_with_transformed_target_regressor(base_model, log_transformer)
+            training_results = train_model(model, model_name, X_train, y_train)
+            predictions, prediction_results = make_predictions(model, model_name, X_test)
             evaluation_results = evaluate_predictions(y_test, predictions, model_name)
 
             results[model_name] = {**training_results, **prediction_results, **evaluation_results}
