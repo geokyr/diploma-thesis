d79d6fa90cc3f897874524c09a08da59df5723f9
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-07-25 02:07:47 +0300

    fixed enums

--- thesis/common/enums.py
+++ thesis/common/enums.py
@@ -5,24 +5,3 @@ class SimulationScenario(StrEnum):
     TRAIN = "train"
     TEST = "test"
     RAIN = "rain"
-
-
-# TODO
-class Recipe(StrEnum):
-    BASE = "base"
-    BASE_RAIN = "base-rain"
-    RAIN = "rain"
-
-
-# TODO
-class Split(StrEnum):
-    TRAIN = "train"
-    TEST = "test"
-
-
-# TODO
-class Model(StrEnum):
-    LINEAR_REGRESSION = "linear_regression"
-    XGBOOST = "xgboost"
-    LIGHTGBM = "lightgbm"
-    CATBOOST = "catboost"
