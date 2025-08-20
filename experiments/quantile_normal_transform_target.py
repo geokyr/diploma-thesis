27b19452d33170ad916297e96d76b14d5f45f824
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-08-20 19:17:31 +0300

    added box cox, log and quantile normal transform target experiments

--- experiments/quantile_normal_transform_target.py
+++ experiments/quantile_normal_transform_target.py
@@ -1,47 +1,55 @@
+from thesis.common.data import generate_trips, load_fcd_dataset, preprocess_fcd_dataset
 from thesis.common.logger import setup_logger
-from thesis.eta.data import generate_trips, load_fcd_dataset, preprocess_fcd_dataset
-from thesis.eta.experiment import initialize_experiment, save_model, save_results
+from thesis.eta.data import ensure_dataset_is_valid
+from thesis.eta.experiment import ETAEvaluation, ETAExperiment, save_model
 from thesis.eta.features import create_quantile_normal_transformer, split_features_and_target
-from thesis.eta.models import get_baseline_models, wrap_with_transformed_target_regressor
-from thesis.eta.pipeline import evaluate_predictions, make_predictions, train_model
-from thesis.eta.specs import build_scenario_specs
+from thesis.eta.models import ModelType, create_model, wrap_with_transformed_target_regressor
+from thesis.eta.pipeline import evaluate_predictions, get_stratified_kfold_cv, make_predictions, train_model
+from thesis.eta.results import build_cv_results, build_model_results, save_results
 
 
 def main() -> None:
-    experiment_name = "quantile_normal_transform_target"
-    artifacts_dir, logs_dir, _, results_dir = initialize_experiment(experiment_name)
-    logger = setup_logger(experiment_name, logs_dir)
+    experiment = ETAExperiment(__file__, ETAEvaluation.RESEARCH)
+    logger = setup_logger(experiment.name, experiment.logs_dir)
+
+    logger.info(f"Running {experiment.name} experiment with {experiment.evaluation} evaluation")
+
+    ensure_dataset_is_valid(experiment.train_path)
+    fcd_train_raw = load_fcd_dataset(experiment.train_path)
+    fcd_train = preprocess_fcd_dataset(fcd_train_raw)
+    trips_train = generate_trips(fcd_train)
+    X_train, y_train = split_features_and_target(trips_train)
 
     transformer = create_quantile_normal_transformer()
+    skf, stratify_key = get_stratified_kfold_cv(y_train)
+    results = {}
 
-    scenario_specs = build_scenario_specs()
+    for model_type in ModelType:
+        base_model = create_model(model_type)
+        model = wrap_with_transformed_target_regressor(base_model, transformer)
 
-    for spec in scenario_specs.values():
-        logger.info(f"Starting scenario {spec.scenario_name}")
+        per_fold_results = []
 
-        fcd_train = load_fcd_dataset(spec.train_path)
-        fcd_test = load_fcd_dataset(spec.test_path)
-        fcd_train = preprocess_fcd_dataset(fcd_train)
-        fcd_test = preprocess_fcd_dataset(fcd_test)
-        trips_train = generate_trips(fcd_train)
-        trips_test = generate_trips(fcd_test)
+        for train_index, val_index in skf.split(X_train, stratify_key):
+            X_train_fold, X_val_fold = X_train.iloc[train_index], X_train.iloc[val_index]
+            y_train_fold, y_val_fold = y_train.iloc[train_index], y_train.iloc[val_index]
 
-        X_train, y_train = split_features_and_target(trips_train)
-        X_test, y_test = split_features_and_target(trips_test)
+            training_results = train_model(model, model_type, X_train_fold, y_train_fold)
+            predictions, prediction_results = make_predictions(model, model_type, X_val_fold)
+            evaluation_results = evaluate_predictions(y_val_fold, predictions, model_type)
 
-        models = get_baseline_models()
-        results = {}
+            fold_results = build_model_results(training_results, prediction_results, evaluation_results)
+            per_fold_results.append(fold_results)
 
-        for model_name, base_model in models.items():
-            model = wrap_with_transformed_target_regressor(base_model, transformer)
-            training_results = train_model(model, model_name, X_train, y_train)
-            predictions, prediction_results = make_predictions(model, model_name, X_test)
-            evaluation_results = evaluate_predictions(y_test, predictions, model_name)
+        results[model_type] = build_cv_results(per_fold_results)
 
-            results[model_name] = {**training_results, **prediction_results, **evaluation_results}
-            save_model(model, model_name, spec.scenario_name, artifacts_dir)
+        logger.info(f"Training final {model_type} model on all training data")
+        final_base_model = create_model(model_type)
+        final_model = wrap_with_transformed_target_regressor(final_base_model, transformer)
+        train_model(final_model, model_type, X_train, y_train)
+        save_model(final_model, model_type, experiment.models_dir)
 
-        save_results(results, spec.scenario_name, results_dir)
+    save_results(results, experiment.results_dir)
 
 
 if __name__ == "__main__":
