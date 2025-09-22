57b04bbb89f460db513bce67bc75007c5c6ccaf9
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-09-22 11:05:34 +0300

    simplified parquet data loader class

--- thesis/predictor/services/data_loader.py
+++ thesis/predictor/services/data_loader.py
@@ -1,21 +1,37 @@
+"""Parquet data loader."""
+
 from pathlib import Path
 
 import pandas as pd
 
-from thesis.common.config import TRIPS_PARQUET_FILENAME
-from thesis.common.service import PlatformServiceConfig
+from thesis.common.config import TIME_START_COLUMN, TRIPS_PARQUET_FILENAME
 
 
-class TripsDataLoader:
-    def __init__(self, service_config: PlatformServiceConfig | None = None) -> None:
-        self._cfg = service_config or PlatformServiceConfig()
+class ParquetDataLoader:
+    """
+    Parquet data loader.
+    """
 
-    def _resolve_path(self) -> Path:
-        return self._cfg.data_dir / TRIPS_PARQUET_FILENAME
+    def __init__(self, data_dir: Path) -> None:
+        self._data_parquet_path = data_dir / TRIPS_PARQUET_FILENAME
 
-    def load_window(self, start_ts: float, end_ts: float) -> pd.DataFrame:
-        path = self._resolve_path()
+    def load_window(self, start_timestamp: int, end_timestamp: int) -> pd.DataFrame:
+        """
+        Load a window of parquet data.
+
+        Args:
+            start_timestamp (int): Start timestamp.
+            end_timestamp (int): End timestamp.
+
+        Returns:
+            pd.DataFrame: Window of parquet data.
+        """
         return pd.read_parquet(
-            path,
-            filters=[[("time_start", ">=", start_ts), ("time_start", "<", end_ts)]],
+            self._data_parquet_path,
+            filters=[
+                [
+                    (TIME_START_COLUMN, ">=", start_timestamp),
+                    (TIME_START_COLUMN, "<", end_timestamp),
+                ]
+            ],
         )
