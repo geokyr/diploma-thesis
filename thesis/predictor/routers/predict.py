309e0f7edc8e81bf0e510eba942a8ca49e5f66fa
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-09-22 17:11:59 +0300

    added docstring for predict batch method

--- thesis/predictor/routers/predict.py
+++ thesis/predictor/routers/predict.py
@@ -10,6 +10,16 @@ predict_router = APIRouter()
 
 @predict_router.post("/batch", response_model=PredictionBatchResponse)
 def predict_batch(req: PredictionBatchRequest, request: Request) -> PredictionBatchResponse:
+    """
+    Predict a batch of data.
+
+    Args:
+        req (PredictionBatchRequest): Request for batch predictions.
+        request (Request): FastAPI request.
+
+    Returns:
+        PredictionBatchResponse: Response for batch predictions.
+    """
     predictor: Predictor = request.app.state.predictor
 
     return predictor.predict_window(
