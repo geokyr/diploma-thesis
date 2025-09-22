044d8ed9237a20900fd9f1b6ae7d4a0fcd6d71ca
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-09-22 17:08:48 +0300

    removed metadata parsing on model manager

--- thesis/predictor/services/model_manager.py
+++ thesis/predictor/services/model_manager.py
@@ -1,37 +1,11 @@
 """Model manager for a predictor with simple versioning."""
 
-import json
-from dataclasses import dataclass
 from pathlib import Path
 
 import joblib
 from sklearn.base import BaseEstimator
 
-from thesis.common.config import DEFAULT_VERSION, METADATA_FILENAME, MODEL_FILENAME
-from thesis.common.enums import MLTask
-from thesis.eta.models import ModelType
-
-
-@dataclass(frozen=True, slots=True)
-class ModelMetadata:
-    """
-    Metadata for a model.
-
-    Attributes:
-        ml_task (MLTask): The task the model is for.
-        version (str): The version of the model.
-        created_at (str): The timestamp the model was created at.
-        model (ModelType): The model type.
-        start_timestamp (int): The training data start timestamp used to train the model.
-        end_timestamp (int): The training data end timestamp used to train the model.
-    """
-
-    ml_task: MLTask
-    version: str
-    created_at: str
-    model: ModelType
-    start_timestamp: int
-    end_timestamp: int
+from thesis.common.config import DEFAULT_VERSION, MODEL_FILENAME
 
 
 class ModelManager:
@@ -44,7 +18,6 @@ class ModelManager:
 
     def __init__(self, models_dir: Path) -> None:
         self._models_dir = models_dir
-        self._metadata: ModelMetadata | None = None
         self._version: str | None = None
         self.model: BaseEstimator | None = None
 
@@ -80,20 +53,10 @@ class ModelManager:
         self.model = joblib.load(model_path)
         self._version = model_path.parent.name
 
-        metadata_path = model_path.with_name(METADATA_FILENAME)
-
-        try:
-            if metadata_path.exists():
-                metadata = json.loads(metadata_path.read_text(encoding="utf-8"))
-                self._metadata = ModelMetadata(**metadata)
-        except Exception:
-            self._metadata = None
-
     def close(self) -> None:
         """
         Close the model manager.
         """
         self._models_dir = None
-        self._metadata = None
         self._version = None
         self.model = None
