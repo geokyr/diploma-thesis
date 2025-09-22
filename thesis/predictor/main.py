712b7857fa5821b0a00fb125e11ba653870c2011
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-09-23 00:44:29 +0300

    renamed close methods to clear and removed parquet from data loader

--- thesis/predictor/main.py
+++ thesis/predictor/main.py
@@ -8,7 +8,7 @@ from thesis.common.logger import setup_logger
 from thesis.common.schemas import HealthResponse
 from thesis.common.service import PlatformService, PlatformServiceConfig
 from thesis.predictor.routers.predict import predict_router
-from thesis.predictor.services.data_loader import ParquetDataLoader
+from thesis.predictor.services.data_loader import DataLoader
 from thesis.predictor.services.model_manager import ModelManager
 from thesis.predictor.services.predictor import Predictor
 
@@ -19,32 +19,32 @@ logger = setup_logger(config.service, config.logs_dir)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
-        parquet_data_loader = ParquetDataLoader(config.data_dir)
-        model_manager = ModelManager(config.models_dir)
-        predictor = Predictor(parquet_data_loader, model_manager)
+        data_loader = DataLoader(data_dir=config.data_dir)
+        model_manager = ModelManager(models_dir=config.models_dir)
+        predictor = Predictor(data_loader=data_loader, model_manager=model_manager)
 
-        app.state.parquet_data_loader = parquet_data_loader
+        app.state.data_loader = data_loader
         app.state.model_manager = model_manager
         app.state.predictor = predictor
         yield
     finally:
         predictor: Predictor = getattr(app.state, "predictor", None)
         model_manager: ModelManager = getattr(app.state, "model_manager", None)
-        parquet_data_loader: ParquetDataLoader = getattr(app.state, "parquet_data_loader", None)
+        data_loader: DataLoader = getattr(app.state, "data_loader", None)
 
         if predictor is not None:
-            predictor.close()
+            predictor.clear()
         if model_manager is not None:
-            model_manager.close()
-        if parquet_data_loader is not None:
-            parquet_data_loader.close()
+            model_manager.clear()
+        if data_loader is not None:
+            data_loader.clear()
 
         if hasattr(app.state, "predictor"):
             delattr(app.state, "predictor")
         if hasattr(app.state, "model_manager"):
             delattr(app.state, "model_manager")
-        if hasattr(app.state, "parquet_data_loader"):
-            delattr(app.state, "parquet_data_loader")
+        if hasattr(app.state, "data_loader"):
+            delattr(app.state, "data_loader")
 
 
 app = FastAPI(title="Platform Predictor Service", version="1.0.0", lifespan=lifespan)
