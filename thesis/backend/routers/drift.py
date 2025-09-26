7df9819b7297bfe4e8a2c5b1b19e41be51fca997
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-09-26 17:07:48 +0300

    capitalization

--- thesis/backend/routers/drift.py
+++ thesis/backend/routers/drift.py
@@ -11,13 +11,13 @@ drift_router = APIRouter()
 @drift_router.get("/status", response_model=DriftErrorsResponse)
 async def get_drift_status(request: Request) -> DriftErrorsResponse:
     """
-    Get the status of all ML Tasks from drift service.
+    Get the status of all ML tasks from drift service.
 
     Args:
         request: Request object.
 
     Returns:
-        DriftErrorsResponse: Status of all ML Tasks.
+        DriftErrorsResponse: Status of all ML tasks.
     """
     drift_client: DriftClient = request.app.state.drift_client
     return await drift_client.get_status()
