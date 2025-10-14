eda01cb78af56cfd97e9a9e5f8b293a848a9f853
George Kyriakopoulos <imgeorgekyr@gmail.com>
2025-10-14 14:20:54 +0300

    add final model training script for LightGBM with best hyperparameters and save model output

--- experiments/final_model.py
+++ experiments/final_model.py
@@ -2,8 +2,8 @@ from thesis.common.data import generate_trips, load_fcd_dataset, preprocess_fcd_
 from thesis.common.logger import setup_logger
 from thesis.eta.data import ensure_dataset_is_valid
 from thesis.eta.experiment import ETAEvaluation, ETAExperiment
-from thesis.eta.features import add_all_features, create_quantile_transformer, split_features_and_target
-from thesis.eta.models import ModelType, create_model, save_model, wrap_with_transformed_target_regressor
+from thesis.eta.features import FeatureCalibratorETA, split_features_and_target
+from thesis.eta.models import ModelType, create_model, save_model
 from thesis.eta.pipeline import train_model
 
 
@@ -17,29 +17,29 @@ def main() -> None:
     fcd_train_raw = load_fcd_dataset(experiment.train_path)
     fcd_train = preprocess_fcd_dataset(fcd_train_raw)
     trips_train_raw = generate_trips(fcd_train)
-    trips_train = add_all_features(trips_train_raw)
+
+    calibrator = FeatureCalibratorETA.from_train_trips(trips_train_raw)
+    trips_train = calibrator.transform(trips_train_raw)
     X_train, y_train = split_features_and_target(trips_train)
 
-    transformer = create_quantile_transformer()
     best_params = {
         "max_depth": 14,
-        "n_estimators": 1400,
-        "num_leaves": 120,
-        "learning_rate": 0.03171569744780489,
-        "subsample": 0.632766493612861,
-        "colsample_bytree": 0.8591507913974749,
-        "min_child_samples": 43,
-        "min_split_gain": 0.00098582036201482,
-        "reg_lambda": 0.01019913851411128,
-        "reg_alpha": 0.0,
+        "n_estimators": 1100,
+        "num_leaves": 104,
+        "learning_rate": 0.043311390827708816,
+        "subsample": 0.9584344495013392,
+        "colsample_bytree": 0.6921069310836445,
+        "min_child_samples": 38,
+        "min_split_gain": 1.2499234477818435e-07,
+        "reg_alpha": 6.790340597036079e-06,
+        "reg_lambda": 3.467795781008837e-05,
     }
 
     model_type = ModelType.LIGHTGBM_REGRESSOR
     logger.info(f"Training final {model_type} with best parameters on all training data")
-    base_model = create_model(model_type, **best_params)
-    final_model = wrap_with_transformed_target_regressor(base_model, transformer)
-    train_model(final_model, model_type, X_train, y_train)
-    save_model(final_model, model_type, experiment.models_dir)
+    model = create_model(model_type, **best_params)
+    train_model(model, model_type, X_train, y_train)
+    save_model(model, model_type, experiment.models_dir)
 
 
 if __name__ == "__main__":
